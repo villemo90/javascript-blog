68b903d431fb1932ae665f92568def0a22e832d8
Sylwia Sniezko-Blocka <galka.sylwestra@gmail.com>
2020-05-02 18:31:11 +0200

    Add links active script for script.js

--- js/script.js
+++ js/script.js
@@ -1,33 +1,30 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
-    console.log(activeLink);
   }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
 
 
-
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts .active');
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
-    console.log(activeArticle);
   }
 
   /* get 'href' attribute from the clicked link */
-
   /* find the correct article using the selector (value of 'href' attribute) */
-
   /* add class 'active' to the correct article */
 }
 
