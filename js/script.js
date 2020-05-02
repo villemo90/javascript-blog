cc5ccee20b6ce9850e49f99bc9415c6fe414fe8d
Sylwia Sniezko-Blocka <galka.sylwestra@gmail.com>
2020-05-02 18:08:12 +0200

    Add remove script for script.js

--- js/script.js
+++ js/script.js
@@ -5,16 +5,30 @@ function titleClickHandler(event){
   console.log(event);
 
   /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
- /* add class 'active' to the clicked link */
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+    console.log(activeLink);
+  }
 
- /* remove class 'active' from all articles */
+  /* add class 'active' to the clicked link */
 
- /* get 'href' attribute from the clicked link */
 
- /* find the correct article using the selector (value of 'href' attribute) */
 
- /* add class 'active' to the correct article */
+  /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts .active');
+
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+    console.log(activeArticle);
+  }
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
 }
 
 const links = document.querySelectorAll('.titles a');
