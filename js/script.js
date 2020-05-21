1857bf77d549c507d52f3720ab4c06cabbf55e99
villemo90 <galka.sylwestra@gmail.com>
2020-05-21 17:40:17 +0200

    Add get article id script

--- js/script.js
+++ js/script.js
@@ -52,15 +52,22 @@ const optArticleSelector = '.post',
 function generateTitleLinks() {
   /* remove contents of titleList */
   const titleList = document.querySelector(optTitleListSelector);
+
   titleList.innerHTML = '';
-  
+
   /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
 
-    /* get the article id */
-
+  for(let article of articles) {
+  /* get the article id */
+    const articleId = article.getAttribute('id');
+  }
     /* find the title element */
 
     /* get the title from the title element */
+
+
+
 
     /* create HTML of the link */
 
